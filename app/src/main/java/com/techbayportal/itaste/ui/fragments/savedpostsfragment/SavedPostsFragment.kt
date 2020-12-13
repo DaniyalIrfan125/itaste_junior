fcb37ecb969d3dba045a6c1b5be8418ad6f63799
daniyal irfan <daniyal@techbayportal.com>
2020-12-13 14:57:59 -0800

    saved posts fragment completed

---
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/savedpostsfragment/SavedPostsFragment.kt
@@ -0,0 +1,67 @@
+package com.techbayportal.itaste.ui.fragments.savedpostsfragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.techbayportal.itaste.BR
+import com.techbayportal.itaste.R
+import com.techbayportal.itaste.baseclasses.BaseFragment
+import com.techbayportal.itaste.databinding.LayoutSavedpostsBinding
+import com.techbayportal.itaste.ui.fragments.profilefragment.adapter.PostsRecyclerAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.layout_savedposts.*
+
+@AndroidEntryPoint
+class SavedPostsFragment : BaseFragment<LayoutSavedpostsBinding, SavedPostsViewModel>() {
+    override val layoutId: Int
+        get() = R.layout.layout_savedposts
+    override val viewModel: Class<SavedPostsViewModel>
+        get() = SavedPostsViewModel::class.java
+    override val bindingVariable: Int
+        get() = BR.viewModel
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        recycler_posts.adapter = PostsRecyclerAdapter(
+            listOf<Int>(
+                R.drawable.img_food_second,
+                R.drawable.img_food_first,
+                R.drawable.img_food_second,
+                R.drawable.img_food_first,
+                R.drawable.img_food_second,
+                R.drawable.img_food_first,
+                R.drawable.img_food_second,
+                R.drawable.img_food_first,
+                R.drawable.img_food_second,
+                R.drawable.img_food_first,
+                R.drawable.img_food_second,
+                R.drawable.img_food_first,
+                R.drawable.img_food_second,
+                R.drawable.img_food_first,
+                R.drawable.img_food_second,
+                R.drawable.img_food_first,
+                R.drawable.img_food_second,
+                R.drawable.img_food_first,
+                R.drawable.img_food_second,
+                R.drawable.img_food_first
+            )
+        )
+        recycler_posts.layoutManager =
+            GridLayoutManager(context, 3, LinearLayoutManager.VERTICAL, false)
+        recycler_posts.autoFitColumns(110)
+    }
+
+
+    fun RecyclerView.autoFitColumns(columnWidth: Int) {
+        val displayMetrics = this.context.resources.displayMetrics
+        val noOfColumns =
+            ((displayMetrics.widthPixels / displayMetrics.density) / columnWidth).toInt()
+        this.layoutManager = GridLayoutManager(this.context, noOfColumns)
+    }
+
+}
