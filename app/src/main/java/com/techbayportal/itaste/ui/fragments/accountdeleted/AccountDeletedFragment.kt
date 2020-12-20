689b439012e8326ce7def1852deec55549cbd1a9
khanadnankhan <khanadnan496@gmail.com>
2020-12-20 16:58:11 +0500

    - Account Deleted Fragment update ~View model added ~xml warnings removed

--- app/src/main/java/com/techbayportal/itaste/ui/fragments/accountdeleted/AccountDeletedFragment.kt
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/accountdeleted/AccountDeletedFragment.kt
@@ -5,21 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.techbayportal.itaste.BR
 import com.techbayportal.itaste.R
+import com.techbayportal.itaste.baseclasses.BaseFragment
+import com.techbayportal.itaste.databinding.FragmentAccountDeletedBinding
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+class AccountDeletedFragment : BaseFragment<FragmentAccountDeletedBinding, AccountDeletedViewModel>() {
 
-class AccountDeletedFragment : Fragment() {
+    override val layoutId: Int
+        get() = R.layout.fragment_delete_account
+    override val viewModel: Class<AccountDeletedViewModel>
+        get() = AccountDeletedViewModel::class.java
+    override val bindingVariable: Int
+        get() = BR.viewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_deleted, container, false)
-    }
+
 
 }
---
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/accountdeleted/AccountDeletedViewModel.kt
@@ -0,0 +1,6 @@
+package com.techbayportal.itaste.ui.fragments.accountdeleted
+
+import com.techbayportal.itaste.baseclasses.BaseViewModel
+
+class AccountDeletedViewModel : BaseViewModel() {
+}
