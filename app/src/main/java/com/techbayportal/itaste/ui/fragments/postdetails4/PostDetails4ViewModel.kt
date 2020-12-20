22b8815f03b7e69307a338de94cff9a13f42cc61
khanadnankhan <khanadnan496@gmail.com>
2020-12-20 18:20:13 +0500

    - Post Details 4 Fragment update ~View model & View Binding added

---
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/postdetails4/PostDetails4ViewModel.kt
@@ -0,0 +1,6 @@
+package com.techbayportal.itaste.ui.fragments.postdetails4
+
+import com.techbayportal.itaste.baseclasses.BaseViewModel
+
+class PostDetails4ViewModel :BaseViewModel() {
+}
--- app/src/main/java/com/techbayportal/itaste/ui/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/settings/SettingsFragment.kt
@@ -1,17 +1,13 @@
 package com.techbayportal.itaste.ui.fragments.settings
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+
 import com.techbayportal.itaste.BR
 import com.techbayportal.itaste.R
 import com.techbayportal.itaste.baseclasses.BaseFragment
-import com.techbayportal.itaste.baseclasses.BaseViewModel
 import com.techbayportal.itaste.databinding.FragmentSettingsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class SettingsFragment : BaseFragment<FragmentSettingsBinding, SettingsViewModel>() {
     override val layoutId: Int
         get() = R.layout.fragment_settings
