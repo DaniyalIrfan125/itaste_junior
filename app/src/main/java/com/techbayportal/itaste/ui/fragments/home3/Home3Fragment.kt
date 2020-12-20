370df27a283094880713e92b0e561750d8b5fa01
khanadnankhan <khanadnan496@gmail.com>
2020-12-20 17:53:59 +0500

    - Home 3 Fragment update ~View model & View Binding added

--- app/src/main/java/com/techbayportal/itaste/ui/fragments/home3/Home3Fragment.kt
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/home3/Home3Fragment.kt
@@ -5,24 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.techbayportal.itaste.BR
 import com.techbayportal.itaste.R
+import com.techbayportal.itaste.baseclasses.BaseFragment
+import com.techbayportal.itaste.databinding.FragmentHome3Binding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+class Home3Fragment : BaseFragment<FragmentHome3Binding, Home3ViewModel>() {
 
-class Home3Fragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home3, container, false)
-    }
+    override val layoutId: Int
+        get() = R.layout.fragment_home3
+    override val viewModel: Class<Home3ViewModel>
+        get() = Home3ViewModel::class.java
+    override val bindingVariable: Int
+        get() = BR.viewModel
 
 
 }
---
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/home3/Home3ViewModel.kt
@@ -0,0 +1,6 @@
+package com.techbayportal.itaste.ui.fragments.home3
+
+import com.techbayportal.itaste.baseclasses.BaseViewModel
+
+class Home3ViewModel : BaseViewModel() {
+}
