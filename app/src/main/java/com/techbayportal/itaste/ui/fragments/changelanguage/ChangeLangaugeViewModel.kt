3ab65201cc2308feb2e4dac9a2fc29150b72b881
khanadnankhan <khanadnan496@gmail.com>
2020-12-21 01:05:46 +0500

    -Package name updated changelanguage to changelanguagefragment -View model name Spelling corrected

--- app/src/main/java/com/techbayportal/itaste/ui/fragments/changelanguage/ChangeLangaugeViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.techbayportal.itaste.ui.fragments.changelanguage
-
-import com.techbayportal.itaste.baseclasses.BaseViewModel
-
-class ChangeLangaugeViewModel :BaseViewModel() {
-}
---
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/changelanguagefragment/ChangeLanguageViewModel.kt
@@ -0,0 +1,6 @@
+package com.techbayportal.itaste.ui.fragments.changelanguagefragment
+
+import com.techbayportal.itaste.baseclasses.BaseViewModel
+
+class ChangeLanguageViewModel :BaseViewModel() {
+}
