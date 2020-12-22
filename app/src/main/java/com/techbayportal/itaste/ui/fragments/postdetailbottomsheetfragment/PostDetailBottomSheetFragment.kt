244c0f7cd25f245eea8d813b596fcc0170e23a17
AdnanKhan <khanadnan496@gmail.com>
2020-12-22 11:54:32 -0800

    - PostDetailBottomSheet bottom sheet fragment extend from Bottom sheet Fragment - added margins in xml

--- app/src/main/java/com/techbayportal/itaste/ui/fragments/postdetailbottomsheetfragment/PostDetailBottomSheetFragment.kt
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/postdetailbottomsheetfragment/PostDetailBottomSheetFragment.kt
@@ -1,5 +1,10 @@
 package com.techbayportal.itaste.ui.fragments.postdetailbottomsheetfragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.techbayportal.itaste.BR
 import com.techbayportal.itaste.R
 import com.techbayportal.itaste.baseclasses.BaseFragment
@@ -7,13 +12,22 @@ import com.techbayportal.itaste.databinding.FragmentPostDetailBottomSheetBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class PostDetailBottomSheetFragment : BaseFragment<FragmentPostDetailBottomSheetBinding, PostDetailBottomSheetFragmentViewModel>() {
-    override val layoutId: Int
-        get() = R.layout.fragment_post_detail_bottom_sheet
-    override val viewModel: Class<PostDetailBottomSheetFragmentViewModel>
-        get() = PostDetailBottomSheetFragmentViewModel::class.java
-    override val bindingVariable: Int
-        get() = BR.viewModel
+class PostDetailBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        var view = inflater.inflate(R.layout.fragment_post_detail_bottom_sheet, container, false)
+
+        return view
+    }
 
 
 }
