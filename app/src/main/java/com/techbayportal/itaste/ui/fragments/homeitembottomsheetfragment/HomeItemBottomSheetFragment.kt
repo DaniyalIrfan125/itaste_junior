6f2d1fc6721575aea7219577e986f909ea50f351
AdnanKhan <khanadnan496@gmail.com>
2020-12-22 11:41:11 -0800

    - Home item bottom sheet fragment extend from Bottom sheet Fragment - added margins in xml

--- app/src/main/java/com/techbayportal/itaste/ui/fragments/homeitembottomsheetfragment/HomeItemBottomSheetFragment.kt
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/homeitembottomsheetfragment/HomeItemBottomSheetFragment.kt
@@ -1,5 +1,10 @@
 package com.techbayportal.itaste.ui.fragments.homeitembottomsheetfragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.techbayportal.itaste.BR
 import com.techbayportal.itaste.R
 import com.techbayportal.itaste.baseclasses.BaseFragment
@@ -7,14 +12,22 @@ import com.techbayportal.itaste.databinding.FragmentHomeItemBottomSheetBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeItemBottomSheetFragment : BaseFragment<FragmentHomeItemBottomSheetBinding, HomeItemBottomSheetViewModel>() {
+class HomeItemBottomSheetFragment : BottomSheetDialogFragment() {
 
-    override val layoutId: Int
-        get() = R.layout.fragment_home_item_bottom_sheet
-    override val viewModel: Class<HomeItemBottomSheetViewModel>
-        get() = HomeItemBottomSheetViewModel::class.java
-    override val bindingVariable: Int
-        get() = BR.viewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        var view = inflater.inflate(R.layout.fragment_home_item_bottom_sheet, container, false)
+
+        return view
+    }
 
 
 }
