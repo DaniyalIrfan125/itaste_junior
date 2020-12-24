3ff19108bd9e86dd734385a83fa5656321107e3a
daniyal irfan <daniyal@techbayportal.com>
2020-12-24 14:47:00 -0800

    for got pass and otp verification validations added

--- app/src/main/java/com/techbayportal/itaste/ui/fragments/forgotpasswordfragment/ForgotPasswordFragment.kt
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/forgotpasswordfragment/ForgotPasswordFragment.kt
@@ -1,5 +1,10 @@
 package com.techbayportal.itaste.ui.fragments.forgotpasswordfragment
 
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import com.techbayportal.itaste.BR
@@ -9,6 +14,7 @@ import com.techbayportal.itaste.databinding.LayoutForgotpasswordfragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.layout_forgotpasswordfragment.*
 import kotlinx.android.synthetic.main.layout_forgotpasswordfragment.btn_next
+import kotlinx.android.synthetic.main.layout_loginfragment.*
 import kotlinx.android.synthetic.main.layout_otpverificationfragment.*
 
 @AndroidEntryPoint
@@ -20,11 +26,41 @@ class ForgotPasswordFragment : BaseFragment<LayoutForgotpasswordfragmentBinding,
     override val bindingVariable: Int
         get() = BR.viewModel
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        fieldTextWatcher()
+    }
+
+    private fun fieldTextWatcher() {
+        ed_forgotPassword.doOnTextChanged { text, start, before, count ->
+            tv_errorForgotPassword.visibility = View.GONE
+            ed_forgotPassword.background =
+                ContextCompat.getDrawable(requireContext(), R.drawable.ed_states)
+        }
+    }
+
+    fun validationsCheck(){
+        if (!TextUtils.isEmpty(ed_forgotPassword.text)) {
+            Navigation.findNavController(btn_next).navigate(R.id.action_forgotPasswordFragment_to_otpverificationFragment)
+        } else {
+            tv_errorForgotPassword.visibility = View.VISIBLE
+            ed_forgotPassword.background =
+                ContextCompat.getDrawable(requireContext(), R.drawable.ed_errorboundary)
+            tv_errorForgotPassword.text = "Please write Username!"
+        }
+
+    }
+
     override fun subscribeToNavigationLiveData() {
         super.subscribeToNavigationLiveData()
 
         mViewModel.onNextButtonClicked.observe(this, Observer {
-            Navigation.findNavController(btn_next).navigate(R.id.action_forgotPasswordFragment_to_otpverificationFragment)
+
+            validationsCheck()
+
         })
 
         mViewModel.onBackButtonClicked.observe(this, Observer {
--- app/src/main/java/com/techbayportal/itaste/ui/fragments/otpverificationfragment/OtpverificationFragment.kt
+++ app/src/main/java/com/techbayportal/itaste/ui/fragments/otpverificationfragment/OtpverificationFragment.kt
@@ -1,5 +1,10 @@
 package com.techbayportal.itaste.ui.fragments.otpverificationfragment
 
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import com.techbayportal.itaste.BR
@@ -8,7 +13,9 @@ import com.techbayportal.itaste.baseclasses.BaseFragment
 import com.techbayportal.itaste.databinding.LayoutOtpverificationfragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.layout_changepasswordfragment.*
+import kotlinx.android.synthetic.main.layout_forgotpasswordfragment.*
 import kotlinx.android.synthetic.main.layout_otpverificationfragment.*
+import kotlinx.android.synthetic.main.layout_otpverificationfragment.btn_next
 
 
 @AndroidEntryPoint
@@ -22,11 +29,35 @@ class OtpverificationFragment :
         get() = BR.viewModel
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fieldTextWatcher()
+
+    }
+
+
+    private fun fieldTextWatcher() {
+        otp_view.doOnTextChanged { text, start, before, count ->
+            tv_otpError.visibility = View.GONE
+        }
+    }
+
+    fun validateFields(){
+        if (!TextUtils.isEmpty(otp_view.text)) {
+            Navigation.findNavController(btn_next).navigate(R.id.action_otpverificationFragment_to_changePasswordFragment)
+        } else {
+            tv_otpError.visibility = View.VISIBLE
+            tv_otpError.text = "Please write Otp!"
+        }
+    }
+
     override fun subscribeToNavigationLiveData() {
         super.subscribeToNavigationLiveData()
 
         mViewModel.onNextButtonClicked.observe(this, Observer {
-            Navigation.findNavController(btn_next).navigate(R.id.action_otpverificationFragment_to_changePasswordFragment)
+            validateFields()
+
         })
 
         mViewModel.onBackButtonClicked.observe(this, Observer {
